20ffd61bdbc83c6a479d8c1a2d80296458384e7f
Remy <fevrer@localhost.localdomain>
2015-10-28 14:58:31 +0100

    remplacement plume2

--- plume.sh
+++ plume.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-continue=true
 
-function arret {
-    echo $line | egrep 'exit';
-    continue=false
-}
-
-function affichage {
-    echo $line | egrep '^GET' && echo '(HTTP/1.0,200,"OK")'
-}
-
-while $continue
+#Tant que la ressource demandée n'est pas égal à "exit" on continue
+while [ "$methode" != "GET" ] || [ "$ressource" != "exit" ]
 do
-    read line
-    echo $continue
-    echo $line | affichage && arret; echo "reçu";
+    #On lit la ligne et on enregistre dans methode et ressource les valeurs correspondante
+    read methode ressource;
+    echo "reçu";
+    #Si la methode est égale à "GET" c'est OK
+    if [ "$methode" == "GET" ]
+    then
+	echo '(HTTP/1.0,200,"OK")';
+	echo "ressource = " $ressource;
+    #Sinon erreur
+    else
+	echo '(HTTP/1.0,405,"Method Not Allowed")';
+    fi
 done
-
-
-
-
 
