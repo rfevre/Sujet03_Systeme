303f3d33b82890ca03ceb07687f25a24bd5630be
Remy <fevrer@localhost.localdomain>
2015-10-31 15:12:11 +0100

    modification modifie-dans-wiki

--- remplace-dans-wiki.sh
+++ remplace-dans-wiki.sh
@@ -5,14 +5,14 @@ read corps;
 
 resultat=$(cat $1 | sed -n '1h;1!H;${;g;s/--DEBUT_REMPLACEMENT'.*'--FIN_REMPLACEMENT--/'"$corps"'/g;p;}');
 
-while (echo $resultat | grep "\*" >2)
+while (echo -e $resultat | grep "\*" >2)
 do
-    resultat=$(echo $resultat | sed '${s/'"\*"'/'"<strong>"'/;s/'"\*"'/'"<\/strong>"'/;}');
+    resultat=$(echo -e $resultat | sed '${s/'"\*"'/'"<strong>"'/;s/'"\*"'/'"<\/strong>"'/;}');
 done
 
-while (echo $resultat | grep "\"" >2)
+while (echo -e $resultat | grep "\"" >2)
 do
-    resultat=$(echo $resultat | sed '${s/'"\""'/'"<em>"'/;s/'"\""'/'"<\/em>"'/;}');
+    resultat=$(echo -e $resultat | sed '${s/'"\""'/'"<em>"'/;s/'"\""'/'"<\/em>"'/;}');
 done
 
-echo $resultat;
+echo -e $resultat;
